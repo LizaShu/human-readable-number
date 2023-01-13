ce60a98067720036c748ccdafc6f4b2100cd698c
LizaShu <lizashu0904@gmail.com>
2023-01-13 14:14:58 +0300

    feat: pass all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    var numberToWords = require('number-to-words');
+    const c = numberToWords.toWords(number);
+    const v = c.replace(/./g, chr => chr === '-' ? ' ' : chr);
+    return v;
 }
